aaff14937dd56e3da22a84213ef8e20b1db88442
rheiformes <rhearai77@gmail.com>
2023-04-27 11:31:54 -0500

    Initial Commit

---
+++ IP15_Mynance/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IP15_Mynance
+//
+//  Created by Rai, Rhea on 4/27/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
